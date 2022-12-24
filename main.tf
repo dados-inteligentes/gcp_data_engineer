cb0908edba3d4a666b140a01c729991aa305f71b
João <jeduardo.info@gmail.com>
2022-12-24 15:15:17 -0300

    add workflow.yaml

---
+++ main.tf
@@ -0,0 +1,5 @@
+
+resource "google_storage_bucket" "gcs_bucket" {
+  location = ""
+  name     = "test-bucket-random-00"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "abiding-topic-368217"
+region = "us-central1"
